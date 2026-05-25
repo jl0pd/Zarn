2fc9a3c31e7fa47c898e20d6d4cdf80106591378
Denis Proshutinskii <jl0pd@outlook.com>
2026-05-25 23:14:36 +0700

    Check registered interfaces

--- Zarn/RpcServiceCollectionExtensions.cs
+++ Zarn/RpcServiceCollectionExtensions.cs
@@ -6,10 +6,23 @@ public static class RpcServiceCollectionExtensions
 {
     public static IServiceCollection AllowRemoteConnection<T>(this IServiceCollection serviceCollection)
     {
+        return serviceCollection.AllowRemoteConnection(typeof(T));
+    }
+
+    public static IServiceCollection AllowRemoteConnection(this IServiceCollection serviceCollection, Type type)
+    {
+        ArgumentNullException.ThrowIfNull(serviceCollection);
+        ArgumentNullException.ThrowIfNull(type);
+
+        if (!type.IsInterface)
+        {
+            throw new ArgumentException("Only interfaces can be remotely connected", nameof(type));
+        }
+
         var descr = serviceCollection.First(x => x.ServiceType == typeof(AllowedRemoteConnections));
 
         var conns = (AllowedRemoteConnections)descr.ImplementationInstance!;
-        conns.Add(typeof(T));
+        conns.Add(type);
 
         return serviceCollection;
     }
